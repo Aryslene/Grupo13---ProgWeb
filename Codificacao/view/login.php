6e919518fce7c6d608bb18e194ec3bd8786cbc7b
WalquiriaLopes <walquiria.lopes11@gmail.com>
2018-11-29 19:48:47 -0200

    Add files via upload

---
+++ Codificacao/view/login.php
@@ -0,0 +1,47 @@
+
+
+
+<html>
+  <head>
+
+  <link href="login.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+  </head>
+<body id="LoginForm">
+<div class="container">
+<h1 class="form-heading">Control Hotel Login</h1>
+<div class="login-form">
+<div class="main-div">
+    <div class="panel">
+   <h2>Seja bem-vindo.</h2>
+   <p>Por favor, entre com seu email e senha</p>
+   </div>
+    <form id="Login">
+
+        <div class="form-group">
+
+
+            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+
+        </div>
+
+        <div class="form-group">
+
+            <input type="password" class="form-control" id="inputPassword" placeholder="Senha">
+
+        </div>
+
+        <button type="submit" class="btn btn-primary">Login</button>
+        <div ckass="naopossuicadastro"><p>Não possui cadastro?</p></div>
+        <button type="submit" class="btn btn-primary">Novo Cadastro</button>
+
+    </form>
+    </div>
+
+</div></div>
+
+
+</body>
+</html>
