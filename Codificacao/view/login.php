f579e1d99754c4b33fbe3dd357c010f2b7b8ec86
WalquiriaLopes <walquiria.lopes11@gmail.com>
2018-12-01 18:50:28 -0200

    Update login.php

--- Codificacao/view/login.php
+++ Codificacao/view/login.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<!--
+*Esse documento faz parte de uma aplicação desenvolvida na matéria de ProgWeb-2018/2 na FACOM, 
+*disciplina lecionada pela Profª Jane Eleutério
+*Seu uso é permitido para fins acadêmicos, todavia mantendo a referência de autoria.
+*@author Aryslene S. Bitencourt - Acadêmica de Engenharia de Software
+*@author Bruna Luzia A. Rodrigues - Acadêmica de Engenharia de Software
+*@author Danielle L. M. G. Benites - Acadêmica de Engenharia de Software
+*@author Walquiria L. Lopes - Acadêmica de Engenharia de Software
+*Framework Bootstrap 4
+-->
 
 
 <html>
@@ -7,17 +17,19 @@
   <link href="login.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+
   </head>
 <body id="LoginForm">
 <div class="container">
 <h1 class="form-heading">Control Hotel Login</h1>
 <div class="login-form">
 <div class="main-div">
+  <!-- Aqui encontra-se os locais para entrada de login e senha -->
     <div class="panel">
    <h2>Seja bem-vindo.</h2>
    <p>Por favor, entre com seu email e senha</p>
    </div>
+  <!-- Formulário do login e senha -->
     <form id="Login">
 
         <div class="form-group">
